47c27bc521da62fb87c18db5e4e51b4d081c2996
morshedulmunna <morshedulmunna1@gmail.com>
2022-07-28 13:29:59 +0600

    Done All Free React Ui compinent

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Tooltip = ({
   children,
   tooltipsText,
+  primary = "bg-primary",
   secondary,
   gray,
   dark,
@@ -39,10 +40,11 @@ const Tooltip = ({
                 `absolute right-full top-1/2 z-20 mr-3 -translate-y-1/2 whitespace-nowrap rounded  py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100`) ||
               (position === "bottom" &&
                 `absolute top-full left-1/2 z-20 mt-3 -translate-x-1/2 whitespace-nowrap rounded  py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100`)
-            }  bg-primary ${
+            }   ${
+              (primary ? `bg-dark` : ``) ||
               (secondary && `bg-secondary`) ||
               (gray && `bg-body-color`) ||
-              (dark && `bg-dark`) ||
+              (dark ? `bg-primary` : ``) ||
               (warning && `bg-warning`) ||
               (danger && `bg-danger`) ||
               (success && `bg-success`) ||
@@ -59,17 +61,18 @@ const Tooltip = ({
                   `absolute right-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45 rounded-sm`) ||
                 (position === "bottom" &&
                   `absolute top-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm`)
-              }   bg-primary ${
+              }    ${
+                (primary ? `bg-dark` : ``) ||
                 (secondary && `bg-secondary`) ||
                 (gray && `bg-body-color`) ||
-                (dark && `bg-dark`) ||
+                (dark ? `bg-primary` : ``) ||
                 (warning && `bg-warning`) ||
                 (danger && `bg-danger`) ||
                 (success && `bg-success`) ||
                 (info && `bg-info`)
               }`}
             ></span>
-            Tooltip Text
+            {tooltipsText}
           </div>
         </div>
       </div>
